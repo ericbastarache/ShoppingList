9433c21b671bb42618c218dca7c7239cbf91b4f1
ericb <eric_bastarache@hotmail.com>
2017-03-10 18:38:29 -0500

    Updated shopping list component with preliminary data

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ListItem } from '../list-item';
+
 @Component({
   selector: 'slist-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  items: ListItem[] = [];
+  item = new ListItem('Eric', 2);
   constructor() { }
 
   ngOnInit() {
